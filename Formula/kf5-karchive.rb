86c47e97d504113faf6bd36d15bcd379a3c1e153
JakkuSakura <qjk2001@gmail.com>
2025-03-20 16:59:52 +0800

    Add kf5-karchive formula to Brewfile and create kf5-karchive.rb

---
+++ Formula/kf5-karchive.rb
@@ -0,0 +1,25 @@
+require_relative "../lib/cmake"
+
+class Kf5KArchive < Formula
+  desc "Process launcher to speed up launching KDE applications"
+  homepage "https://api.kde.org/frameworks/karchive/html/index.html"
+  url "https://download.kde.org/stable/frameworks/5.111/karchive-5.111.0.tar.xz"
+  head "https://invent.kde.org/frameworks/karchive.git", branch: "master"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "extra-cmake-modules" => [:build, :test]
+  depends_on "kdoctools" => :build
+  depends_on "ninja" => :build
+
+  def install
+    system "cmake", *kde_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    prefix.install "build/install_manifest.txt"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(KF5Archive REQUIRED)")
+    system "cmake", ".", "-Wno-dev"
+  end
+end
